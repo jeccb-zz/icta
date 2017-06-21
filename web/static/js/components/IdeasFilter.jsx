5d6abaf4f86e3e0cd2a218399e1376ac059c99b9
Geninho Bonadio <gebonadio@gmail.com>
2017-06-21 17:26:42 -0300

    Add panel to filter component and rename label status class

--- web/static/js/components/IdeasFilter.jsx
+++ web/static/js/components/IdeasFilter.jsx
@@ -3,28 +3,32 @@ import { Translate, I18n } from 'react-redux-i18n';
 import { connect } from 'react-redux';
 
 const IdeasFilter = ({filter, onChange, onClickStatus}) => (
-  <div className="row">
-    <div className="col-xs-12 col-md-7">
-      <div className="form-group">
-        <label htmlFor="filter-text"><Translate value="filter" /></label>
-        <input type="text" className="form-control" placeholder={I18n.t('filter')} value={filter.text} onChange={onChange} />
-      </div>
-    </div>
-    <div className="col-md-5 col-xs-12">
-      <div className="form-group">
-        <div className="row">
-          <div className="col-xs-12">
-            <label htmlFor="filter-text"><Translate value="status" /></label>
+  <div className="panel panel-default">
+    <div className="panel-body">
+      <div className="row">
+        <div className="col-xs-12 col-md-8">
+          <div className="form-group">
+            <label htmlFor="filter-text"><Translate value="filter" /></label>
+            <input type="text" className="form-control" placeholder={I18n.t('filter')} value={filter.text} onChange={onChange} />
           </div>
         </div>
-        <div className="filters-status">
-          { [ 'new', 'planned', 'in_progress', 'done' ].map( (status) => (
-            <div className="filter" key={status}>
-              <span className={`label ${filter.status.includes(status) ? `label-status-${status.replace('_','-')}` : 'label-default'}`} onClick={onClickStatus(status)}>
-                <Translate value={`idea.statuses.${status}`} />
-              </span>
+        <div className="col-xs-12 col-md-4">
+          <div className="form-group">
+            <div className="row">
+              <div className="col-xs-12">
+                <label htmlFor="filter-text"><Translate value="status" /></label>
+              </div>
             </div>
-          ))}
+            <div className="filters-status">
+              { [ 'new', 'planned', 'in_progress', 'done' ].map( (status) => (
+                <div className="filter" key={status}>
+                  <span className={`label ${filter.status.includes(status) ? `status-${status.replace('_','-')}` : 'label-default'}`} onClick={onClickStatus(status)}>
+                    <Translate value={`idea.statuses.${status}`} />
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
